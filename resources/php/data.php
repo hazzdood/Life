36089dc69385868f2f678af06c4f63710c6db0c1
Harry Jacka <hazz.jack@gmail.com>
2017-08-30 18:32:39 +1000

    scraped multiuser functionality

--- resources/php/data.php
+++ resources/php/data.php
@@ -2,25 +2,26 @@
 
 if ($_GET['value'] == 'reset' || $_GET['value'] == 'restart')
 {
-	$userFilePath = realpath("./users/Harry.html"); // get the path to the file
+	$userFilePath = realpath("./user.html"); // get the path to the file
 	$userFile     = fopen($userFilePath, "w+");     // open the file for overwriting and reading
 
 	fwrite($userFile, "<span class='title'>Figure out a way to escape the place you are in</span><br>\n<br>\n\n");
 }
 elseif ($_GET['value'] == '')
 {
-	$userFilePath = realpath("./users/Harry.html"); // get the path to the file
+	$userFilePath = realpath("./user.html"); // get the path to the file
 	$userFile     = fopen($userFilePath, "r");      // open the file for append writing and reading
 }
 else
 {
-	$userFilePath = realpath("./users/Harry.html"); // get the path to the file
+	$userFilePath = realpath("./user.html"); // get the path to the file
 	$userFile     = fopen($userFilePath, "a+");     // open the file for append writing and reading
 
 	fwrite($userFile, "> " . $_GET["value"] . "<br>\n");
 }
 
 rewind($userFile);
+
 $user = fread($userFile, filesize($userFilePath)); // read the contest of the file
 
 $map =
