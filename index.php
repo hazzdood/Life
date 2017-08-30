ddb47105959ac26dd92b0405de5150a3c0009706
Harry Jacka <hazz.jack@gmail.com>
2017-08-30 12:47:41 +1000

    added styling to main page

--- index.php
+++ index.php
@@ -1,33 +1,52 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Figure out a way to escape the place you are in</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> <!-- import bootstrap css -->
+	<link rel="stylesheet" href="./resources/css/main.css">
+</head>
+<body>
+	<div class="container" style="width: 706px;">
+		<nav class="navbar navbar-inverse" role="navigation">
+			<div class="container-fluid">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a href="#" class="navbar-brand">Figure out a way to escape the place you are in</a>
+				</div>
 
-require './resources/php/data.php';
-require './resources/php/layout.php';
-require './resources/php/view.php';
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse navbar-ex1-collapse">
+					<ul class="nav navbar-nav">
+					</ul>
+					<ul class="nav navbar-nav navbar-right">
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Menu <b class="caret"></b></a>
+							<ul class="dropdown-menu">
+								<li><a href="./index.php?a=restart">Restart</a></li>
+								<li><a href="./index.php?a=switch-user">Change User</a></li>
+							</ul>
+						</li>
+					</ul>
+				</div><!-- /.navbar-collapse -->
+			</div>
+		</nav>
 
-$locationName = 'start';
-$location     = null;
-$actionName   = null;
-$action       = null;
+		<div class="content">
+			<span class="title">Figure out a way to escape the place you are in</span><br>
+			<br>
+		</div>
+		<div class="input">
+			> <input type="text" class="textInput" name="input">
+		</div>
+	</div>
 
-if (isset($_GET['location']))
-{
-	$locationName = $_GET['location'];
-}
-
-$location = $map[$locationName];
-
-if (isset($_GET['action']))
-{
-	$actionName = $_GET['action'];
-
-	if (isset($location['actions'][$actionName]))
-	{
-		$newLocationName = $location['actions'][$actionName];
-
-		$location = $map[$newLocationName];
-	}
-}
-
-renderLayout(['content' => getGameView($location)]);
-
-?>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> <!-- import jquery -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> <!-- import bootstrap js -->
+</body>
+</html>
