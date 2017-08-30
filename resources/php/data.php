d1a13478b260072e5a79c41dc06db67e1fd3c64d
Harry Jacka <hazz.jack@gmail.com>
2017-08-30 20:55:54 +1000

    renamed some variables

--- resources/php/data.php
+++ resources/php/data.php
@@ -2,27 +2,27 @@
 
 if ($_GET['value'] == 'reset' || $_GET['value'] == 'restart')
 {
-	$userFilePath = realpath("./user.html"); // get the path to the file
-	$userFile     = fopen($userFilePath, "w+");     // open the file for overwriting and reading
+	$historyFilePath = realpath("./user.html"); // get the path to the file
+	$historyFile     = fopen($historyFilePath, "w+");     // open the file for overwriting and reading
 
-	fwrite($userFile, "<span class='title'>Figure out a way to escape the place you are in</span><br>\n<br>\n\n");
+	fwrite($historyFile, "<span class='title'>Figure out a way to escape the place you are in</span><br>\n<br>\n\n");
 }
 elseif ($_GET['value'] == '')
 {
-	$userFilePath = realpath("./user.html"); // get the path to the file
-	$userFile     = fopen($userFilePath, "r");      // open the file for append writing and reading
+	$historyFilePath = realpath("./user.html"); // get the path to the file
+	$historyFile     = fopen($historyFilePath, "r");      // open the file for append writing and reading
 }
 else
 {
-	$userFilePath = realpath("./user.html"); // get the path to the file
-	$userFile     = fopen($userFilePath, "a+");     // open the file for append writing and reading
+	$historyFilePath = realpath("./user.html"); // get the path to the file
+	$historyFile     = fopen($historyFilePath, "a+");     // open the file for append writing and reading
 
-	fwrite($userFile, "> " . $_GET["value"] . "<br>\n");
+	fwrite($historyFile, "> " . $_GET["value"] . "<br>\n");
 }
 
-rewind($userFile);
+rewind($historyFile);
 
-$user = fread($userFile, filesize($userFilePath)); // read the contest of the file
+$history = fread($historyFile, filesize($historyFilePath)); // read the contest of the file
 
 $map =
 [
