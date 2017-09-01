7ded687e6481520b65a1de4faa91b0de9eb6b524
Harry Jacka <hazz.jack@gmail.com>
2017-09-01 12:08:17 +1000

    added data manager and file

--- resources/php/data.php
+++ resources/php/data.php
@@ -2,20 +2,30 @@
 
 if ($_GET['value'] == 'reset' || $_GET['value'] == 'restart')
 {
-	$historyFilePath = realpath("./user.html"); // get the path to the file
-	$historyFile     = fopen($historyFilePath, "w+");     // open the file for overwriting and reading
+	$historyFilePath = realpath("./user/history.html"); // get the path to the file
+	$historyFile     = fopen($historyFilePath, "w+");   // open the file for overwriting and reading
+
+	$dataFilePath = realpath("./user/history.html"); // get the path to the file
+	$dataFile     = fopen($historyFilePath, "w+");   // open the file for overwriting and reading
 
 	fwrite($historyFile, "<span class='title'>Figure out a way to escape the place you are in</span><br>\n<br>\n\n");
+	fwrite($dataFile, "{\n	\"location\": \"start\",\n	\"inventory\":\n	[\n		\"a low IQ\",\n	],\n}\n");
 }
 elseif ($_GET['value'] == '')
 {
-	$historyFilePath = realpath("./user.html"); // get the path to the file
-	$historyFile     = fopen($historyFilePath, "r");      // open the file for append writing and reading
+	$historyFilePath = realpath("./user/history.html"); // get the path to the file
+	$historyFile     = fopen($historyFilePath, "r");    // open the file for append writing and reading
+
+	$dataFilePath = realpath("./user/history.html"); // get the path to the file
+	$dataFile     = fopen($historyFilePath, "a+");   // open the file for overwriting and reading
 }
 else
 {
-	$historyFilePath = realpath("./user.html"); // get the path to the file
-	$historyFile     = fopen($historyFilePath, "a+");     // open the file for append writing and reading
+	$historyFilePath = realpath("./user/history.html"); // get the path to the file
+	$historyFile     = fopen($historyFilePath, "a+");   // open the file for append writing and reading
+
+	$dataFilePath = realpath("./user/history.html"); // get the path to the file
+	$dataFile     = fopen($historyFilePath, "a+");   // open the file for overwriting and reading
 
 	fwrite($historyFile, "> " . $_GET["value"] . "<br>\n");
 }
@@ -23,6 +33,9 @@ else
 rewind($historyFile);
 
 $history = fread($historyFile, filesize($historyFilePath)); // read the contest of the file
+
+$data = fread($dataFile, filesize($historyFilePath)); // read the contest of the file
+$data = json_decode($data, true);                     // decode the json data to an assoc array
 
 $map =
 [
