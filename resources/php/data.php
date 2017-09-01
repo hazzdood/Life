b75405e4dc437bb6e175cd5cd51e080c47e04e98
Harry Jacka <hazz.jack@gmail.com>
2017-09-01 12:43:33 +1000

    fixed file loading

--- resources/php/data.php
+++ resources/php/data.php
@@ -2,30 +2,38 @@
 
 if ($_GET['value'] == 'reset' || $_GET['value'] == 'restart')
 {
+	$dataFilePath = realpath("./user/data.json"); // get the path to the file
+	$dataFile     = fopen($dataFilePath, "w+");   // open the file for overwriting and reading
+
+	rewind($dataFile);
+	fwrite($dataFile, "{\n	\"location\": \"start\",\n	\"inventory\":\n	[\n		\"a low IQ\",\n	],\n}\n");
+
+	$data = json_decode("{\n	\"location\": \"start\",\n	\"inventory\":\n	[\n		\"a low IQ\",\n	],\n}\n", true); // decode the json data to an assoc array
+
 	$historyFilePath = realpath("./user/history.html"); // get the path to the file
 	$historyFile     = fopen($historyFilePath, "w+");   // open the file for overwriting and reading
 
-	$dataFilePath = realpath("./user/history.html"); // get the path to the file
-	$dataFile     = fopen($historyFilePath, "w+");   // open the file for overwriting and reading
-
 	fwrite($historyFile, "<span class='title'>Figure out a way to escape the place you are in</span><br>\n<br>\n\n");
-	fwrite($dataFile, "{\n	\"location\": \"start\",\n	\"inventory\":\n	[\n		\"a low IQ\",\n	],\n}\n");
 }
 elseif ($_GET['value'] == '')
 {
+	$dataFilePath = realpath("./user/data.json");              // get the path to the file
+	$dataFile     = fopen($dataFilePath, "a+");                // open the file for overwriting and reading
+	$data         = fread($dataFile, filesize($dataFilePath)); // read the contest of the file
+	$data         = json_decode($data, true);                  // decode the json data to an assoc array
+
 	$historyFilePath = realpath("./user/history.html"); // get the path to the file
 	$historyFile     = fopen($historyFilePath, "r");    // open the file for append writing and reading
-
-	$dataFilePath = realpath("./user/history.html"); // get the path to the file
-	$dataFile     = fopen($historyFilePath, "a+");   // open the file for overwriting and reading
 }
 else
 {
+	$dataFilePath = realpath("./user/data.json");              // get the path to the file
+	$dataFile     = fopen($dataFilePath, "a+");                // open the file for overwriting and reading
+	$data         = fread($dataFile, filesize($dataFilePath)); // read the contest of the file
+	$data         = json_decode($data, true);                  // decode the json data to an assoc array
+
 	$historyFilePath = realpath("./user/history.html"); // get the path to the file
 	$historyFile     = fopen($historyFilePath, "a+");   // open the file for append writing and reading
-
-	$dataFilePath = realpath("./user/history.html"); // get the path to the file
-	$dataFile     = fopen($historyFilePath, "a+");   // open the file for overwriting and reading
 
 	fwrite($historyFile, "> " . $_GET["value"] . "<br>\n");
 }
@@ -33,9 +41,6 @@ else
 rewind($historyFile);
 
 $history = fread($historyFile, filesize($historyFilePath)); // read the contest of the file
-
-$data = fread($dataFile, filesize($historyFilePath)); // read the contest of the file
-$data = json_decode($data, true);                     // decode the json data to an assoc array
 
 $map =
 [
