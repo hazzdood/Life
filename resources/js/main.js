e7f9b2930ce1bb87821ec71dce7f988098873eaf
Harry Jacka <hazz.jack@gmail.com>
2017-09-08 12:23:18 +1000

    comments

--- resources/js/main.js
+++ resources/js/main.js
@@ -2,35 +2,35 @@ function submit(textBox, value = '')
 {
 	delay = 5;
 
-	if (value != '')
+	if (value != '') // if a value was sent strait to the function
 	{
-		get = { value: value };
+		get = { value: value }; // collect the contents into an array
 
-		$.get("./action.php", get, function(data)
+		$.get("./action.php", get, function(data) // and send it to action.php
 		{
-			$(".content").html(data);
+			$(".content").html(data); // then put the response into the content span
 		});
-
-		setTimeout(function () { window.scrollTo(0,document.body.scrollHeight) }, delay);
 	}
-	else
+	else // if not
 	{
-		if(event.keyCode == 13)
+		if(event.keyCode == 13) // and the return key was pressed
 		{
-			get = { value: textBox.value };
+			get = { value: textBox.value }; // collect the contents of the text box into an array
 
-			$.get("./action.php", get, function(data)
+			$.get("./action.php", get, function(data) // and send it to action.php
 			{
-				$(".content").html(data);
+				$(".content").html(data); // then put the response into the content span
 			});
 
-			textBox.value = '';
+			textBox.value = ''; // clear the text box
 
-			setTimeout(function () { window.scrollTo(0,document.body.scrollHeight) }, delay);
+			setTimeout(function () { window.scrollTo(0,document.body.scrollHeight) }, delay); // and scroll the screen
 		}
 	}
 }
 
+// send an empty request to load the history when the page loads
+//
 get = { value: '' };
 
 $.get("./action.php", get, function(data)
